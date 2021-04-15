635e22ac0abde4451809a28676e5aba75959bc92
0712023 <0712023@hanmail.net>
2021-04-15 19:20:04 +0900

    test

---
+++ test.py
@@ -0,0 +1,34 @@
+import os
+import paho.mqtt.client as mqtt # Import the MQTT library
+import time # The time library is useful for delays
+from math import cos, sin, pi, floor
+from adafruit_rplidar import RPLidar
+
+# Setup the RPLidar
+PORT_NAME = '/dev/ttyUSB0'
+lidar = RPLidar(None, PORT_NAME)
+
+# used to scale data to fit on the screen
+max_distance = 0
+
+# setup mqtt connection
+client = mqtt.Client("makerio_mqtt")
+client.connect("192.168.168.162", 1883)
+client.loop_start()
+
+def process_data(data):
+    client.publish("test", '['+','.join(str(e) for e in data)+']')
+    pass
+
+scan_data = [0]*360
+
+try:
+    for scan in lidar.iter_scans():
+        for (_, angle, distance) in scan:
+            scan_data[min([359, floor(angle)])] = distance
+        process_data(scan_data)
+
+except KeyboardInterrupt:
+    print('Stoping.')
+lidar.stop()
+lidar.disconnect()
