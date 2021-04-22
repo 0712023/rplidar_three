2079fa56ab8d1fa3a36ea2388826eed50359bb1e
0712023 <0712023@hanmail.net>
2021-04-22 15:45:03 +0900

    rename files

--- 3D_modeling_vue_cli/src/router/router.js
+++ 3D_modeling_vue_cli/src/router/router.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Render from '../views/3D_Modeling.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: '3D_Modeling',
+    component: Render
   }
 ]
 
